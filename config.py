ba9bccb3044517dd076d6a3db119feafc83ef801
Kacper Janas <quar5@wp.pl>
2023-05-30 09:40:16 +0200

    add: basic config values

---
+++ config.py
@@ -0,0 +1,12 @@
+from colorama import Fore
+
+COUNT_CONN = False
+
+AP_IP = "192.168.1.1"
+SUBNET_MASK = 24
+
+KEYWORDS = ["password", "user", "username", "login", "pass", "Username", "Password", "User", "Email"]
+
+GREEN = Fore.GREEN
+RED   = Fore.RED
+RESET = Fore.RESET
--- sniffer.py
+++ sniffer.py
@@ -1,65 +1,68 @@
 import os, sys
 from collections import Counter
+from ipaddress import ip_network
 from scapy.all import sniff, IP, Raw
 from scapy.utils import PcapWriter
 import scapy.layers.http as scapy_http
 
-from colorama import init, Fore
+from colorama import init
 
-COUNT_CONN = False
+from config import AP_IP, SUBNET_MASK, COUNT_CONN, \
+                    RED, GREEN, RESET, \
+                    KEYWORDS
 
+# Initialize colorama
 init()
-GREEN = Fore.GREEN
-RED   = Fore.RED
-RESET = Fore.RESET
-
+# Get network IP
+network_ip = ip_network(f"{AP_IP}/{SUBNET_MASK}", strict=False).network_address
+# Initialize counter
 packet_counts = Counter()
 packet_id = 0
+# Initialize writers
 http_writer = PcapWriter("http.cap", append=True, sync=True)
 keyword_writer = PcapWriter("keyword.cap", append=True, sync=True)
 https_writer = PcapWriter("https.cap", append=True, sync=True)
 
-WORDS = ["password", "user", "username", "login", "pass", "Username", "Password", "User", "Email"]
+
+def is_in_network(IP):
+    net_ip = ip_network(f"{IP}/{SUBNET_MASK}", strict=False).network_address
+    # print(f"@INFO: {net_ip} == {network_ip} -> {net_ip == network_ip}")
+    return (net_ip == network_ip)
+
 
 
 def handle_package(pkt):
     global packet_id
     # Create tuple of Src/Dst
-    key = tuple(sorted([pkt[0][1].src, pkt[0][1].dst]))
+    src_ip = pkt[0][1].src
+    dst_ip = pkt[0][1].dst
     if COUNT_CONN:
-        packet_counts.update([key])
+        packet_counts.update([tuple(sorted([src_ip, dst_ip]))])
 
-    # pkt.show()
+    if is_in_network(src_ip) and pkt.haslayer(scapy_http.HTTPRequest) and pkt.haslayer(Raw):
+        # If packet has not encrypted HTTPRequest it is a HTTP request
+        http_writer.write(pkt)
 
-    if pkt.haslayer(scapy_http.HTTPRequest):
-        if pkt.haslayer(Raw):
-            if pkt.dport == 80:
-                http_writer.write(pkt)
-            elif pkt.dport == 443:
-                https_writer.write(pkt)
-            else:
-                print("[@INFO] - Weird packet spoofed")
+        url = pkt[scapy_http.HTTPRequest].Host.decode() + pkt[scapy_http.HTTPRequest].Path.decode()
+        ip = pkt[IP].src
+        method = pkt[scapy_http.HTTPRequest].Method.decode()
 
-            url = pkt[scapy_http.HTTPRequest].Host.decode() + pkt[scapy_http.HTTPRequest].Path.decode()
-            ip = pkt[IP].src
-            method = pkt[scapy_http.HTTPRequest].Method.decode()
+        print(f"{GREEN}[+] {ip} requested {url} with {method}{RESET}")
 
-            print(f"{GREEN}[+] {ip} requested {url} with {method}{RESET}")
-
-            load = pkt[Raw].load
-            for w in WORDS:
-                if w in str(load):
-                    keyword_writer.write(pkt)
-                    print(f"{RED}[!] Keyword found in packet -- {load.decode()}{RESET}")
-                    break
+        load = pkt[Raw].load
+        for w in KEYWORDS:
+            if w in str(load):
+                keyword_writer.write(pkt)
+                print(f"{RED}[!] Keyword found -- {load.decode()}{RESET}")
+                break
 
     packet_id += 1
-    return f"Packet #{packet_id}: {key[0]} ==> {key[1]}"
+    return f"Packet #{packet_id}: {src_ip} ==> {dst_ip}"
 
 
 def main():
     # Start sniffing
-    sniff(filter="ip and ( port 80 or port 443 )", prn=handle_package)
+    sniff(filter=f"ip and ( port 80 or port 443 ) and not ip src {AP_IP}", prn=handle_package)
     # Print out packet count per A <--> Z address pair
     if COUNT_CONN:
         print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
