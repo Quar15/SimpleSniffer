96473ee46bbfa776a16de9ec355d0c62f6d33a65
Kacper Janas <quar5@wp.pl>
2023-05-30 17:04:25 +0200

    refactor: cleanup and added comments

--- sniffer.py
+++ sniffer.py
@@ -1,6 +1,7 @@
 import os, sys
 from collections import Counter
 from ipaddress import ip_network
+
 from scapy.all import sniff, IP, Raw
 from scapy.utils import PcapWriter
 import scapy.layers.http as scapy_http
@@ -11,8 +12,6 @@ from config import AP_IP, SUBNET_MASK, COUNT_CONN, \
                     RED, GREEN, RESET, \
                     KEYWORDS
 
-# Initialize colorama
-init()
 # Get network IP
 network_ip = ip_network(f"{AP_IP}/{SUBNET_MASK}", strict=False).network_address
 # Initialize counter
@@ -33,39 +32,50 @@ def is_in_network(IP):
 
 def handle_package(pkt):
     global packet_id
-    # Create tuple of Src/Dst
+    # Get source and destination IP from packet
     src_ip = pkt[0][1].src
     dst_ip = pkt[0][1].dst
     if COUNT_CONN:
         packet_counts.update([tuple(sorted([src_ip, dst_ip]))])
 
-    if is_in_network(src_ip) and pkt.haslayer(scapy_http.HTTPRequest) and pkt.haslayer(Raw):
-        # If packet has not encrypted HTTPRequest it is a HTTP request
-        http_writer.write(pkt)
+    # Save packet if it is source ip is in AP network
+    if is_in_network(src_ip):
+        # and if packet has HTTPRequest and Raw layer
+        if pkt.haslayer(scapy_http.HTTPRequest) and pkt.haslayer(Raw):
+            # If packet has not encrypted HTTPRequest it is a HTTP request
+            http_writer.write(pkt)
 
-        url = pkt[scapy_http.HTTPRequest].Host.decode() + pkt[scapy_http.HTTPRequest].Path.decode()
-        ip = pkt[IP].src
-        method = pkt[scapy_http.HTTPRequest].Method.decode()
+            # Decode basic data from packet
+            url = pkt[scapy_http.HTTPRequest].Host.decode() + pkt[scapy_http.HTTPRequest].Path.decode()
+            ip = pkt[IP].src
+            method = pkt[scapy_http.HTTPRequest].Method.decode()
 
-        print(f"{GREEN}[+] {ip} requested {url} with {method}{RESET}")
+            print(f"{GREEN}[+] {ip} requested {url} with {method}{RESET}")
 
-        load = pkt[Raw].load
-        for w in KEYWORDS:
-            if w in str(load):
-                keyword_writer.write(pkt)
-                print(f"{RED}[!] Keyword found -- {load.decode()}{RESET}")
-                break
+            # Load raw data from packet and search if it has any keywords in it
+            load = pkt[Raw].load
+            for w in KEYWORDS:
+                if w in str(load):
+                    keyword_writer.write(pkt)
+                    print(f"{RED}[!] Keyword found -- {load.decode()}{RESET}")
+                    break
+        else:
+            pkt.show2()
+            https_writer.write(pkt)
 
     packet_id += 1
     return f"Packet #{packet_id}: {src_ip} ==> {dst_ip}"
 
 
 def main():
+    # Initialize colorama
+    init()
     # Start sniffing
     sniff(filter=f"ip and ( port 80 or port 443 ) and not ip src {AP_IP}", prn=handle_package)
-    # Print out packet count per A <--> Z address pair
     if COUNT_CONN:
+        # Print out packet count per A <--> Z address pair
         print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
+    
     print("@INFO: Sniffer shutdown")
 
 
